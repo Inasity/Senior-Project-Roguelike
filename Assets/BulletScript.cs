75eefc3f45487eeeadd4ff6aeb56b54dcaadb2d5
Inasity <55552445+Inasity@users.noreply.github.com>
2021-02-23 23:39:53 -0600

    Weapons, UI, and Health
    Added a basic template for weapons, added the health system for the player (UI and variables), and laid the resolution for the UI for the additional variables that will be added down the line.

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (Collider hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+}
