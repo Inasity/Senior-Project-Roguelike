bd8590f761606a8bfbe385d39e9f2703121267f9
Inasity <55552445+Inasity@users.noreply.github.com>
2021-03-02 20:34:29 -0600

    Implementing the basis for level generation

---
+++ Assets/RoomController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class RoomInfo
+{
+    public string name;
+    public int X;
+    public int Y;
+}
+
+public class RoomController : MonoBehaviour
+{
+
+    public static RoomController instance;
+    string currentWorldName = "FirstLevel";
+    RoomInfo currentLoadRoomData;
+    Queue<RoomInfo> loadRoomQueue = new Queue<RoomInfo>();
+    public List<Room> loadedRooms = new List<Room>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
