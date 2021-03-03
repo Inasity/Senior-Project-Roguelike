a864b967051f8f8ef1aeb7b59e0b19b3b20ef2ab
Inasity <55552445+Inasity@users.noreply.github.com>
2021-03-02 19:07:25 -0600

    Bullet additions + bug fixes

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -7,17 +7,24 @@ public class BulletScript : MonoBehaviour
 
     public float speed = 20f;
     public Rigidbody rb;
+    public float gravityTimer = 1.0f;
+    Rigidbody r;
 
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = transform.forward * speed;
+        r = this.gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gravityTimer-= Time.deltaTime;
+        if (gravityTimer <= 0f)
+        {
+            r.useGravity = true;
+        }
     }
 
     void OnTriggerEnter (Collider hitInfo)
